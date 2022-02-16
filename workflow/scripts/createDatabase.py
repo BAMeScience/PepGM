9d7be1f273c254da4d6419f4ee92b466dde6ed70
fkistner <fkistner@sv2218.zit.bam.de>
2022-02-16 18:09:08 +0100

    add snakemake rules for createDatabase.py

--- workflow/scripts/createDatabase.py
+++ workflow/scripts/createDatabase.py
@@ -3,46 +3,37 @@ import gzip
 import subprocess
 from pathlib import Path
 
+import argparse
 import mmh3
 import numpy as np
 from requests import get
 
 
-# preliminaries
-path_to_resources = Path('../../resources/')
-command_to_split = "awk '{print $1}' '../../resources/protacc2taxids_virus.txt' > '../../resources/accessions.txt'; " \
-                   "awk '{print $2}' '../../resources/protacc2taxids_virus.txt' > '../../resources/taxids.txt'"
+parser = argparse.ArgumentParser(description = 'Hash protein accession database')
+parser.add_argument('--input_path', help ='path to database')
+parser.add_argument('--output_path', help ='path where to save hashed database')
+
+args = parser.parse_args()
 
 
-def split_to_accessions_and_taxids(command):
-    """
-    Bash wrapper for splitting two-column file into two files with a single column.
-
-    """
-    subprocess.run(command, shell=True)
-
-
-def hash_database(path, seed=18):
+def hash_database(input_path, output_path, seed=18):
     """
     Perform mmh3 hashing on input database.
     MurmurHash3 is a non-cryptographic hashing algorithm.
     For more information on mmh3 algorithm visit https://pypi.org/project/mmh3/.
 
-    :param path: str, input path to protein accession database (accessions need to be separated by \n)
+    :param output_path:
+    :param input_path:
     :param seed: int, hashing seed: use identical integer for identical hashing results
     :return: database: np.array, hashed database
     """
     # initialize database
-    database = np.empty([sum(1 for _ in open(path))])
+    database = np.empty([sum(1 for _ in open(input_path))])
     # hash protein accessions - this is where the magic happens
-    with open(path, 'r') as f:
+    with open(input_path, 'r') as f:
         for line_num, line in enumerate(f, 1):
             database[line_num-1] = mmh3.hash64(line, signed=False, seed=seed)[0]
-    np.save(path_to_resources / 'accessions_hashed.npy', database)
-    return database
+    np.save(output_path, database)
 
 
-if __name__ == "__main__":
-    split_to_accessions_and_taxids(command_to_split)
-    hash_database(path_to_resources / 'accessions.txt')
-    print('Created database.')
+hash_database(args.input_path, args.output_path)

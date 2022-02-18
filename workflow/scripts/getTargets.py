6fb18435885e06a40cd3f1dd72ddec963f47e80b
fkistner <fkistner@sv2218.zit.bam.de>
2022-02-18 18:22:18 +0100

    make getTargets snakemakeable\

--- workflow/scripts/getTargets.py
+++ workflow/scripts/getTargets.py
@@ -1,5 +1,4 @@
 import argparse
-import datetime
 import linecache
 import os
 
@@ -7,32 +6,34 @@ import mmh3
 import numba as nb
 import numpy as np
 import pandas as pd
-import psutil
+
 
 # argparser preliminaries
 parser = argparse.ArgumentParser(description = 'Find ')
-parser.add_argument('-rq', '--path_to_raw_query', help='path to raw PSM report')
+parser.add_argument('-rq', '--path_to_raw_query', help='path to raw query')
 parser.add_argument('-q', '--path_to_query', help='path to processed query')
 parser.add_argument('-d', '--path_to_database', help='path to hashed database')
-parser.add_argument('-t', '--path_to_mapped_taxids', help='path to results')
+parser.add_argument('-r', '--path_to_mapped_taxids', help='path to results')
+parser.add_argument('-t', '--path_to_taxids', nargs='*')
 args = parser.parse_args()
 
 
-def save_results_to_txt(path, results):
+def save_to_txt(path, results):
     """
     Save results.
 
     :param path: str, where to save results
     :param results: lst, results from database query
     """
-    output = open(path, 'w')
-    [output.write(str(result)) for result in results]
-    output.close()
+    f = open(path, 'w')
+    [f.write(str(result)) for result in results]
+    f.close()
 
 
 def preprocess_query(input_path, output_path):
     """
     Extract protein accessions from PeptideShaker output (PSM report) and save them.
+
     :param input_path: str, input path to raw query
     :param output_path: str, output path
     :return: -
@@ -48,7 +49,10 @@ def preprocess_query(input_path, output_path):
         proteins.append(accession)
     # merge sublists
     proteins = [j for i in [protein.split(',') for protein in proteins] for j in i]
-    save_results_to_txt(output_path, proteins)
+    # write in output file
+    accessions_final = open(output_path, "w")
+    [accessions_final.write(element + "\n") for element in proteins]
+    accessions_final.close()
 
 
 def hash_query(path):
@@ -85,37 +89,31 @@ def query_database(database, query):
     return lst
 
 
-def lines_to_taxids(match, path='../../resources/taxids.txt'):
+def lines_to_taxids(match, path):
     """
     Map matches to taxids.
 
     :param match: lst, index of matches
     :param path: str, path to taxid database
-    :return lst, taxids
+    :return lst, mapped taxids
     """
     taxids = []
     for idx in match:
         taxids.append((linecache.getline(path, idx)))
     taxids_unique = []
     # remove duplicates
-    for taxid in tax_ids:
+    for taxid in taxids:
         if taxid not in taxids_unique:
-            taxids_unique.append(x)
+            taxids_unique.append(taxid)
     return taxids_unique
 
 
-# '/home/fkistner/pepgm/resources/chicken_refseq_Default_PSM_Report.txt'
-# '/home/fkistner/pepgm/results/chicken_refseq_query_accessions.txt'
+# prepare
 preprocess_query(args.path_to_raw_query, args.path_to_query)
-
-# '/home/fkistner/pepgm/results/chicken_refseq_query_accessions.txt'
 query_accessions = hash_query(args.path_to_query)
-
-# '/home/fkistner/pepgm/resources/accessions_hashed.npy'
 database_accessions = np.load(args.path_to_database)
+# lookup
 lookup = query_database(database_accessions, query_accessions)
-taxids = lines_to_taxids(lookup)
-
-# '/home/fkistner/pepgm/results/mapped_taxids.txt'
-save_results_to_txt(args.path_to_mapped_taxids, taxids)
-print('Found targets.')
+taxids = lines_to_taxids(lookup, args.path_to_taxids[0])
+# save
+save_to_txt(args.path_to_mapped_taxids, taxids)
--- workflow/scripts/hashDatabase.py
+++ workflow/scripts/hashDatabase.py
@@ -9,32 +9,32 @@ import numpy as np
 from requests import get
 
 
+# argparser preliminaries
 parser = argparse.ArgumentParser(description = 'Hash protein accession database')
-parser.add_argument('--input_path', help ='path to database')
-parser.add_argument('--output_path', help ='path where to save hashed database')
+parser.add_argument('-i', '--input_path', help ='path to database')
+parser.add_argument('-o', '--output_path', help ='path where to save hashed database')
 args = parser.parse_args()
 
 
-def hash_database(inp, out, seed=18):
+def hash_database(input_path, output_path, seed=18):
     """
     Perform mmh3 hashing on input database.
     MurmurHash3 is a non-cryptographic hashing algorithm.
     For more information on mmh3 algorithm visit https://pypi.org/project/mmh3/.
 
-
-    :param inp: str, input path
-    :param out: str, output path
+    :param input_path: str, input path
+    :param output_path: str, output path
     :param seed: int, hashing seed: use identical integer for identical hashing results
     :return: database: np.array, hashed database
     """
     # initialize database
-    database = np.empty([sum(1 for _ in open(inp))])
+    database = np.empty([sum(1 for _ in open(input_path))])
     # hash protein accessions
-    with open(inp, 'r') as f:
+    with open(input_path, 'r') as f:
         for line_num, line in enumerate(f, 1):
             database[line_num-1] = mmh3.hash64(line, signed=False, seed=seed)[0]
     # save in numpy format
-    np.save(out, database)
+    np.save(output_path, database)
 
 
 hash_database(args.input_path, args.output_path)

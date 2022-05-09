09b0e6326dd5a39f610927c6fbc1d7bacb7783a6
tholstei <tanja.holstein@outlook.com>
2022-05-09 11:27:43 +0200

    added automatic saving of best results, csv output, and corresponding Phylotree rendering

--- workflow/scripts/PhyloTreeView.py
+++ workflow/scripts/PhyloTreeView.py
@@ -1,98 +1,62 @@
 from ete3 import PhyloTree,Tree,TreeStyle,NodeStyle,faces,AttrFace,CircleFace
 from ete3 import NCBITaxa
 import pandas as pd
-import csv
 
-
-'''
-This script plots the PepGM results onto a phylogenetic tree using the ete3 tree visualization tools
-'''
-
-#resultsfile = '/home/tholstei/repos/PepGM_all/PepGM/results/refseqViral/PXD025131_Sars_CoV_2/Prior0.1/chlorocebus_refseqViral_PepGM_Results_a0.01_b0.05_p0.1.csv'
-#out = '/home/tholstei/repos/PepGM_all/PepGM/results/refseqViral/PXD025131_Sars_CoV_2/PhyloTree.png'
-#host = 'chlorocebus'
-
-#resultsfile = '/home/tholstei/repos/PepGM_all/PepGM/results/refseqViral/PXD002936_avian_bronchitis/Prior0.3/chicken_refseqViral_PepGM_Results_a0.1_b0.1_p0.3.csv'
-#out = '/home/tholstei/repos/PepGM_all/PepGM/results/refseqViral/PXD002936_avian_bronchitis/Phylotree.png'
-#host = "gallus gallus"
-
-#resultsfile = '/home/tholstei/repos/PepGM_all/PepGM/results/refseqViral/PXD005104_Herpessimplex_1/Prior0.1/human_refseqViral_PepGM_Results_a0.01_b0.4_p0.1.csv'
-#out = '/home/tholstei/repos/PepGM_all/PepGM/results/refseqViral/PXD005104_Herpessimplex_1/Phylotree.png'
-#host = "homo sapiens"
-
-#resultsfile = '/home/tholstei/repos/PepGM_all/PepGM/results/refseqViral/PXD025130_Sars_CoV_2/Prior0.1/chlorocebus_refseqViral_PepGM_Results_a0.01_b0.05_p0.1.csv'
-#out = '/home/tholstei/repos/PepGM_all/PepGM/results/refseqViral/PXD025130_Sars_CoV_2/PhyloTree.png'
-#host = 'chlorocebus'
-
-resultsfile = '/home/tholstei/repos/PepGM_all/PepGM/results/refseqViral+cowpoxstrains/PXD003013_Cowpox_BR/Prior0.1/human_refseqViral+cowpoxstrains_PepGM_Results_a0.2_b0.01_p0.1.csv'
-out = '/home/tholstei/repos/PepGM_all/PepGM/results/refseqViral+cowpoxstrains/PXD003013_Cowpox_BR/PhyloTree.png'
-host = 'homo sapiens'
 
 ncbi = NCBITaxa()
 
-#get csv results file
-Results = pd.read_csv(resultsfile, names = ['ID','score','type'])
-#remove host taxon from visualization
-HostTaxid = ncbi.get_name_translator([host])[host][0]
-HostTaxidList = [str(i) for i in ncbi.get_descendant_taxa(HostTaxid)]+[str(HostTaxid)]
-        
-#keep only taxa in dataframe
-TaxIDS = Results.loc[Results['type']=='taxon']
-#adjust types
-TaxIDS.loc[:,'score'] = pd.to_numeric(TaxIDS['score'],downcast = 'float')
-#sort accoring to scores
-TaxIDS = TaxIDS.sort_values('score', ascending = False)
-#drop host taxa /descendants of host taxa
-#TaxIDS = TaxIDS[TaxIDS.ID.isin(HostTaxidList)==False]
-TaxIDS.drop(TaxIDS[TaxIDS.ID.isin(HostTaxidList)].index, inplace=True)
+def CreatePhyloTreeView(resultsfile,host,out):
 
-#put the 15 highest scoring taxids into minimal connecting phylotree
-phylotree = ncbi.get_topology(TaxIDS.ID.tolist()[:15])
-#print(phylotree.get_ascii(attributes=["sci_name"]))
-#phylotree = PhyloTree(phylotree.write())
-#print(phylotree.traverse())
-#phylotree.annotate_ncbi_taxa()
+    '''
+    Plots the PepGM results onto a phylogenetic tree using the ete3 tree visualization tools
+    :params resulsfile: str,file with PepGM .csv results
+    :params host: str,host of the virs to be remove from the phylogenetic tree view
+    :params out: str, output path
+    '''
 
-#get a dictionary to translate TaxiDs into Taxa names
-#TaxaNameDict = ncbi.get_taxid_translator(phylotree.get_species())
-#TaxaNames = [TaxaNameDict[int(tax)] for tax in TaxIDS.ID.tolist()[:15]]
+    
+    Results = pd.read_csv(resultsfile, names = ['ID','score','type'])
 
-#write the phylotree into a tree to be able to manipulate nodesizes
+    #remove host taxon from visualization
+    HostTaxid = ncbi.get_name_translator([host])[host][0]
+    HostTaxidList = [str(i) for i in ncbi.get_descendant_taxa(HostTaxid)]+[str(HostTaxid)]
+    
+            
+    #keep only taxa in dataframe
+    TaxIDS = Results.loc[Results['type']=='taxon']
+    #adjust types
+    TaxIDS.loc[:,'score'] = pd.to_numeric(TaxIDS['score'],downcast = 'float')
+    #sort accoring to scores
+    TaxIDS = TaxIDS.sort_values('score', ascending = False)
+    TaxIDS.drop(TaxIDS[TaxIDS.ID.isin(HostTaxidList)].index, inplace=True)
+  
+    
+    #put the 15 highest scoring taxids into minimal connecting phylotree
+    phylotree = ncbi.get_topology(TaxIDS.ID.tolist()[:15])
 
+    #set score as node weights
+    for nodeName in phylotree.iter_leaves():
+        nodeName.add_features(weight=TaxIDS.loc[TaxIDS['ID']== nodeName.name,'score'].iloc[0] *30)
+        #nodeName.spname = TaxaNameDict[int(nodeName.name)]
+ 
+    def layout(node):
+        #if node.is_leaf():
+            # Add node name to laef nodes
+        N = AttrFace("sci_name", fsize=14, fgcolor="black")
+        faces.add_face_to_node(N, node, 0)
+        if "weight" in node.features:
+            # Creates a sphere face whose size is proportional to node's
+            # feature "weight"
+            C = CircleFace(radius=node.weight, color="RoyalBlue", style="sphere")
+            # Let's make the sphere transparent
+            C.opacity = 1
+            # And place as a float face over the tree
+            faces.add_face_to_node(C, node, 0, position="float")
+    
+    ts = TreeStyle()
+    ts.layout_fn = layout
 
-
-#set score as node weights
-for nodeName in phylotree.iter_leaves():
-    nodeName.add_features(weight=TaxIDS.loc[TaxIDS['ID']== nodeName.name,'score'].iloc[0] *30)
-    #nodeName.spname = TaxaNameDict[int(nodeName.name)]
-
-#annote nodes with psecies names
-#for node in phylotree.traverse():
-    #print(node.name)
-    #print(node.sci_name)
-    #node.spname = ncbi.get_name_translator([node.name])[int(node.name)]
-
-
-
-def layout(node):
-    #if node.is_leaf():
-        # Add node name to laef nodes
-    N = AttrFace("sci_name", fsize=14, fgcolor="black")
-    faces.add_face_to_node(N, node, 0)
-    if "weight" in node.features:
-        # Creates a sphere face whose size is proportional to node's
-        # feature "weight"
-        C = CircleFace(radius=node.weight, color="RoyalBlue", style="sphere")
-        # Let's make the sphere transparent
-        C.opacity = 1
-        # And place as a float face over the tree
-        faces.add_face_to_node(C, node, 0, position="float")
-
-ts = TreeStyle()
-ts.layout_fn = layout
-#ts.show_leaf_name = False
-
-phylotree.render(out,tree_style = ts, w=300, units="mm")   
+    phylotree.render(out,tree_style = ts, w=300, units="mm")   
 
 
 
---
+++ workflow/scripts/SearchAnalysisOutputFormatter.py
@@ -0,0 +1,60 @@
+from GridSearchAnalysis import *
+from PhyloTreeView import *
+import pandas as pd
+import os
+import argparse 
+from ete3 import NCBITaxa
+
+parser = argparse.ArgumentParser(description = 'Downstream analysis of the grid search for the PepGM algorithm')
+parser.add_argument('--resultsfolder', required = True, help = 'folder with the results from the beliefpropagation in csv format')
+parser.add_argument('--out', required=True, help ='output png file')
+parser.add_argument('--host', required =True, help = 'name of the host, to be excluded from parameter checked taxa')
+parser.add_argument('--reference_db',required = True, help = 'name of the reference datasbe used to include in the filepath string')
+
+args = parser.parse_args()
+
+ncbi = NCBITaxa()
+
+
+def SaveReducedCSV(filepath,host,output):
+    '''
+    Saves CSV of the 20 best results to a file in the main results folder
+    :param filepath: str, path of cvs to be reduced
+    :param host: str,if included in the search results, the host will be excluded from the results list
+    :param output: str,output path of csv
+    '''
+        
+    HostTaxid = ncbi.get_name_translator([host])[host][0]
+    HostTaxidList = [str(i) for i in ncbi.get_descendant_taxa(HostTaxid)]+[str(HostTaxid)]
+            
+    Results = pd.read_csv(filepath,names = ['ID','score','type'])
+    TaxIDS = Results.loc[Results['type']=='taxon']
+    TaxIDS.loc[:,'score'] = pd.to_numeric(TaxIDS['score'],downcast = 'float')
+    TaxIDS = TaxIDS.sort_values('score', ascending = False)
+    TaxIDS = TaxIDS[TaxIDS.ID.isin(HostTaxidList)==False]
+    TaxIDS.drop(TaxIDS[TaxIDS.ID.isin(HostTaxidList)].index, inplace=True)
+    
+    TaxIDS.to_csv(output)
+           
+
+
+def MoveBestResultsPlot(filepath,out):
+    '''
+    Copies barplot of best identifiedparameter set to main resultsfolder
+    :param filepath: list, the three grid search parameters identified as best for the sample at hand
+    :param out: output path where the file will be copied
+
+    '''
+    os.system('cp '+filepath +' '+out)
+
+
+#analyse the PepGM grid search with an empirical metric, retrieve best parameters
+Parameters = ComputeMetric(args.resultsfolder, args.host, args.out)
+Resultsfile = args.resultsfolder+'/Prior'+str(Parameters[2]) +'/'+ args.reference_db + '_PepGM_Results_a'+str(Parameters[0])+'_b'+str(Parameters[1])+'_p'+str(Parameters[2])+'.csv'
+
+#save the reduced results csv
+SaveReducedCSV(Resultsfile, args.host, args.resultsfolder +'/PepGm_Results.csv')
+MoveBestResultsPlot(Resultsfile,args.resultsfolder+'/PepGM_ResultsPlot.png')
+
+#save a phylogenetic tree view of the PepGm results
+CreatePhyloTreeView(Resultsfile,args.host, args.resultsfolder + '/PhyloTreeView.png')

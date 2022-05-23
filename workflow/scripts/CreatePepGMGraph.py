94b80c1908c3458b6e13cc47cac2bb21676778cf
fkistner <franziska.kistner@bam.de>
2022-05-23 13:33:22 +0200

    integrate master changes

--- workflow/scripts/CreatePepGMGraph.py
+++ workflow/scripts/CreatePepGMGraph.py
@@ -1,37 +1,31 @@
-"""
-This module initializes a taxon graph and fills in peptides and factors.
-"""
+
 import argparse
 from os.path import exists
 from FactorGraphGeneration import *
 
-# argparser preliminaries
-parser = argparse.ArgumentParser(description = 'Initialize graphical model with taxons, peptides and factors.')
-parser.add_argument('--targetTaxa', type = str, nargs = 1,
-                    help ='Path to mapped taxids text file. Format: One taxid per line.')
-parser.add_argument('--PSM_Report', type =str, required =True,
-                    help = 'Path to psm report (peptide shaker ouput).')
-parser.add_argument('--PeptideMapPath',type=str, required =True,
-                    help = 'Path to taxon peptide map. Format: json file')
-parser.add_argument('--out', type = str, required = True,
-                    help = "Output directory and filename.")
+
+
+parser = argparse.ArgumentParser(description = 'Run the PepGM algorithm from command line')
+
+parser.add_argument('--targetTaxa', type = str, nargs = 1, help ='path to the .txt file containing a list of taids to include in the graph, one taxid per line')
+parser.add_argument('--PSM_Report', type =str, required =True, help = 'path to your PSM report txt file (output from peptideshaker)')
+parser.add_argument('--PeptideMapPath',type=str, required =True, help = 'path to where you want to save you taxon-peptide map .json file')#make it so that this works as argument for both functions
+parser.add_argument('--out', type = str, required = True, help = 'path to where you want to save the GraphML file of the factorgraph')
+parser.add_argument('--sourceDB',type = str, nargs ='?', const ='', help = 'name of the DB queried through Entrez')
+
 args = parser.parse_args()
 
-# init taxon networkx graph object
+
+print(args.targetTaxa)
 Taxongraph = TaxonGraph()
 Taxongraph.GetAllLeafTaxaFromTaxids(args.targetTaxa[0])
-
-# fetch taxon data if the peptide map file does not exist yet
+#only fetch TaxonData if the PeptideMap File doesn't exist yet:
 if not exists(args.PeptideMapPath):
     print(exists(args.PeptideMapPath))
     Taxongraph.FetchTaxonData(args.PeptideMapPath, args.sourceDB)
-
-# add peptides from peptide shaker output
-Taxongraph.CreateTaxonPeptidegraphFromPSMresults(args.PeptideMapPath, args.PSM_Report, min_score = 0.001)
-
-# add factors
+Taxongraph.CreateTaxonPeptidegraphFromPSMresults(args.PSM_Report,args.PeptideMapPath,0.001)
+#Taxongraph.CreateExample()
 Factorgraph = FactorGraph()
 Factorgraph.ConstructFromTaxonGraph(Taxongraph)
 CTFactorgraph = GenerateCTFactorGraphs(Factorgraph)
-# save
 CTFactorgraph.SaveToGraphML(args.out)

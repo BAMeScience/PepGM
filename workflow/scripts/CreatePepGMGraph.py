b40df02bd8c9a75a3f8fd6b8d4df56ce15ef0ecc
fkistner <franziska.kistner@bam.de>
2022-05-23 13:09:43 +0200

    clean formatting and documentation

--- workflow/scripts/CreatePepGMGraph.py
+++ workflow/scripts/CreatePepGMGraph.py
@@ -1,43 +1,37 @@
-
+"""
+This module initializes a taxon graph and fills in peptides and factors.
+"""
 import argparse
 from os.path import exists
 from FactorGraphGeneration import *
 
-
-
-parser = argparse.ArgumentParser(description = 'Run the PepGM algorithm from command line')
-
-#parser.add_argument('--targetTaxa', nargs = '*', help ='enter a list of taxa to include in your graphical model')
-parser.add_argument('--targetTaxa', type = str, nargs = 1, help ='path to the .txt file containing a list of taids to include in the graph, one taxid per line')
-parser.add_argument('--PSM_Report', type =str, required =True, help = 'path to your PSM report txt file (output from peptideshaker)')
-parser.add_argument('--PeptideMapPath',type=str, required =True, help = 'path to where you want to save you taxon-peptide map .json file')#make it so that this works as argument for both functions
-parser.add_argument('--out', type = str, required = True, help = 'path to where you want to save the GraphML file of the factorgraph')
-parser.add_argument('--sourceDB',type = str, nargs ='?', const ='', help = 'name of the DB queried through Entrez')
-
+# argparser preliminaries
+parser = argparse.ArgumentParser(description = 'Initialize graphical model with taxons, peptides and factors.')
+parser.add_argument('--targetTaxa', type = str, nargs = 1,
+                    help ='Path to mapped taxids text file. Format: One taxid per line.')
+parser.add_argument('--PSM_Report', type =str, required =True,
+                    help = 'Path to psm report (peptide shaker ouput).')
+parser.add_argument('--PeptideMapPath',type=str, required =True,
+                    help = 'Path to taxon peptide map. Format: json file')
+parser.add_argument('--out', type = str, required = True,
+                    help = "Output directory and filename.")
 args = parser.parse_args()
 
-#targetTaxa ='/home/tholstei/repos/PepGM_all/PepGM/results/PXD025130_Sars_CoV_2/chlorocebus_refseq_mapped_taxids.txt'
-#PeptideMapPath = '/home/tholstei/repos/PepGM_all/PepGM/resources/SampleData/PXD025130_Sars_CoV_2/Sars_automap_taxa.json'
-#PSM_Report = '/home/tholstei/repos/PepGM_all/PepGM/resources/SampleData/PXD025130_Sars_CoV_2/chlorocebus_refseq_Default_PSM_Report.txt'
-#out = '/home/tholstei/repos/PepGM_all/PepGM/resources/SampleData/PXD025130_Sars_CoV_2/refseq_PepGM_graph.graphml'
-#sourceDB = 'all[FILT]'
-
-#targetTaxa = ['herpesviridae','homo sapiens']
-#PeptideMapPath = '/home/tholstei/repos/PepGM_all/PepGM/resources/SampleData/PXD005104_Herpessimplex_1/herpesviridae.json'
-#PSM_Report = '/home/tholstei/repos/PepGM_all/PepGM/resources/SampleData/PXD005104_Herpessimplex_1/human_refseq_Default_PSM_Report.txt'
-#out = '/home/tholstei/repos/PepGM_all/PepGM/resources/SampleData/PXD005104_Herpessimplex_1/human_refseq_PepGM_graph.graphml'
-
-
-print(args.targetTaxa)
+# init taxon networkx graph object
 Taxongraph = TaxonGraph()
 Taxongraph.GetAllLeafTaxaFromTaxids(args.targetTaxa[0])
-#only fetch TaxonData if the PeptideMap File doesn't exist yet:
+
+# fetch taxon data if the peptide map file does not exist yet
 if not exists(args.PeptideMapPath):
     print(exists(args.PeptideMapPath))
     Taxongraph.FetchTaxonData(args.PeptideMapPath, args.sourceDB)
-Taxongraph.CreateTaxonPeptidegraphFromPSMresults(args.PSM_Report,args.PeptideMapPath,0.001)
-#Taxongraph.CreateExample()
+
+# add peptides from peptide shaker output
+Taxongraph.CreateTaxonPeptidegraphFromPSMresults(args.PeptideMapPath, args.PSM_Report, min_score = 0.001)
+
+# add factors
 Factorgraph = FactorGraph()
 Factorgraph.ConstructFromTaxonGraph(Taxongraph)
 CTFactorgraph = GenerateCTFactorGraphs(Factorgraph)
+# save
 CTFactorgraph.SaveToGraphML(args.out)
